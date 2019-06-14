e6616a6ee198892140a1c3801fc9c1b5ff527444
Rafa-MKR2 <rafaeldocarmo2010@gmail.com>
2019-06-14 13:00:58 -0300

    aplicando animações

--- js/config.js
+++ js/config.js
@@ -2,8 +2,8 @@
     
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.autocomplete');
-    var instances = M.Autocomplete.init(elems, 
-   { data: {
+    M.Autocomplete.init(elems,{ 
+     data: {
         "H.P Lovecraft":  'img/img-secao.jpg',
         "Edgar Alan Poe":  'img/img-4.jpg',
         "Isaac Asimov":  'img/img-2.jpg',
@@ -12,5 +12,28 @@
 
         "Rafael D.Carmo":  'img/índice.jpeg',
         "Monteiro Lobato": 'img/MonteiroLobato.jpg'
-      }});
+      }
+    });
+ 
+    var chip = document.querySelectorAll('.chips-autocomplete');
+     M.Chips.init(chip,{
+      autocompleteOptions: {
+        data: {
+          'Apple': null,
+          'Microsoft': null,
+          'Google': null
+        },
+        limit: Infinity,
+        minLength: 1
+      },
+      
+        placeholder: 'Aperte Enter para adicionar uma Tag exemplo: ',
+        secondaryPlaceholder: '+Tag',
+      
+    });
+
+
+    var carousel = document.querySelectorAll('.carousel');
+         M.Carousel.init(carousel);
+ 
   });
